4a4abeeda211f33f21b328981ed66e12554eccaa
runjuu <hi@runjuu.com>
2025-04-24 18:38:36 -0700

    chore: update project.pbxproj

--- Input Source Pro/Utilities/AppKit/NSColor.swift
+++ Input Source Pro/Utilities/AppKit/NSColor.swift
@@ -2,38 +2,6 @@ import AppKit
 import SwiftUI
 
 extension NSColor {
-    static let alignmentIndicator = NSColor(named: "AlignmentIndicatorColor")!
-
-    static let fakeWindow = NSColor(named: "FakeWindowColor")!
-
-    static let close = NSColor(named: "CloseColor")!
-
-    static let maximise = NSColor(named: "MaximiseColor")!
-
-    static let minimise = NSColor(named: "MinimiseColor")!
-
-    static let background = NSColor(named: "Background")!
-
-    static let background1 = NSColor(named: "Background1")!
-
-    static let background2 = NSColor(named: "Background2")!
-
-    static let border = NSColor(named: "BorderColor")!
-
-    static let border2 = NSColor(named: "BorderColor2")!
-
-    static let rainbow1 = NSColor(named: "Rainbow1")!
-
-    static let rainbow2 = NSColor(named: "Rainbow2")!
-
-    static let rainbow3 = NSColor(named: "Rainbow3")!
-
-    static let rainbow4 = NSColor(named: "Rainbow4")!
-
-    static let rainbow5 = NSColor(named: "Rainbow5")!
-
-    static let rainbow6 = NSColor(named: "Rainbow6")!
-
     var color: Color {
         Color(self)
     }
--- Input Source Pro/Utilities/AppKit/NSImage.swift
+++
@@ -1,5 +0,0 @@
-import AppKit
-
-extension NSImage {
-    static let triangle = NSImage(named: "Triangle")!
-}
